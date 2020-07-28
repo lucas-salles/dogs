14ead22ae3218d175141a8603c82afd9a8876e64
Lucas Sales <lucassales398@gmail.com>
2020-07-28 14:53:50 -0300

    API Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './components/Home'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 import Login from './components/Login/Login';
 
 function App() {
