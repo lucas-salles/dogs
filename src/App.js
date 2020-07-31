c503c49e64632dd46ca7b42cef9a7b57ef9f9441
Lucas Sales <lucassales398@gmail.com>
2020-07-31 13:56:05 -0300

    Perfil Usuário

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { UserStorage } from "./UserContext";
 import User from "./components/User/User";
 import ProtectedRoute from "./components/Helper/ProtectedRoute";
 import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
